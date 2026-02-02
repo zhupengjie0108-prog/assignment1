55fdd616cefda7e2206865bb2759cc8f9481409f
a q <25411985@life.hkbu.edu.hk>
2026-02-02 16:41:23 +0800

    data cleaning script

--- clean.py
+++ clean.py
@@ -5,15 +5,15 @@ def main():
     contact_file = sys.argv[1]
     other_file = sys.argv[2]
     output_file = sys.argv[3]
-    
+
     df1 = pd.read_csv(contact_file)
     df2 = pd.read_csv(other_file)
-    
+
     merged = pd.merge(df1, df2, left_on='respondent_id', right_on='id')
-    merged = merged.drop(columns=['id'])  
-    
+    merged = merged.drop(columns=['id'])
+
     merged = merged.dropna()
-    
+
     insurance_mask = merged['job'].str.contains('insurance', case=False)
     merged = merged[~insurance_mask]
 
